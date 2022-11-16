317802985fb28bdc85c742374e0fb2175066d451
Pradeep Singh <pradeep.s@opensenselabs.com>
2022-11-17 01:43:30 +0530

    created preview for form

--- src/FormGenerator.js
+++ src/FormGenerator.js
@@ -1,9 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ElementStore, ReactFormGenerator } from "react-form-builder2";
 
 const FormGenerator = () => {
-  return (
-    <div>FormGenerator</div>
-  )
+    const [previewVisible, setPreviewVisible] = useState(false);
+    const [data, setData] = useState([]);
+
+    useEffect(() =>{
+        ElementStore.subscribe((state) => {
+            setData(state.data)
+        })
+    }, [])
+
+    const showPreview = () => {
+        setPreviewVisible(true);
+      }
+
+      const closePreview = () => {
+        setPreviewVisible(false);
+      }
+
+      const onSubmit = () => {
+          ElementStore.dispatch("updateOrder", data)
+        console.log("onSubmit", data);
+        // Place code to post json data to server here
+      }
+
+      let modalClass = "modal";
+    if (previewVisible) {
+      modalClass += " show d-block";
+    }
+
+    
+    return (
+        <div className="clearfix" style={{ margin: "10px", width: "70%" }}>
+          <h4 className="float-left">Preview</h4>
+          <button
+            className="btn btn-primary float-right"
+            style={{ marginRight: "10px" }}
+            onClick={showPreview}
+          >
+            Preview Form
+          </button>
+          
+  
+          {previewVisible && (
+            <div className={modalClass}>
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <ReactFormGenerator
+                    download_path=""
+                    back_action="/"
+                    back_name="Back"
+                    answer_data={{}}
+                    action_name="Save"
+                    form_action="/"
+                    form_method="POST"
+                    onSubmit={onSubmit}
+                    data={data}
+                  />
+  
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-default"
+                      data-dismiss="modal"
+                      onClick={closePreview}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+  
+         
+        </div>
+      );
 }
 
 export default FormGenerator;
+
+
+
